66a7ece6a68d56f8db86bb1b673b34a3ff690834
pascalypperciel <pypperciel@gmail.com>
2024-06-16 17:58:57 -0400

    variety of stuff

--- code/code.ino
+++ code/code.ino
@@ -9,7 +9,7 @@
 #define DISTANCE_HORIZONTAL 40.00
 #define DISTANCE_VERTICAL   80.00
 #define SIDEWAYS_DELAY_MS   2000
-#define DELAY_BEFORE_TURN   3000
+#define DELAY_BEFORE_TURN   2000
 
 // Constants
 #define FAN_HORI_SPEED      255
@@ -23,11 +23,12 @@
 
 // Debug
 #define RUN_HOVERCRAFT      false
-#define DEBUG_SENSORS       true
+#define DEBUG_SENSORS       false
 #define DEBUG_SERVO         false
 #define DEBUG_FANS          false
 #define DEBUG_LIFT          false
-#define DEBUG_SERVO_SPIN    true
+#define DEBUG_SERVO_SPIN    false
+#define DEBUG_SKIRT         true
 
 // Structures
 typedef struct {
@@ -165,7 +166,7 @@ void SERVO_change_angle(float angle) {
     if (angle < 0) angle = 0;
     if (angle > 180) angle = 180;
 
-    OCR1A = map(angle, 0, 180, 1000, 4500);
+    OCR1A = map(angle, 0, 180, 800, 4300);
     
     if (RUN_HOVERCRAFT) {
         if (angle == SERVO_SIDEWAYS) {
@@ -267,6 +268,12 @@ int main(void) {
             _delay_ms(2000);
             SERVO_change_angle(SERVO_DOWNHILL);
             _delay_ms(2000);
+        }
+
+        if (DEBUG_SKIRT) {
+            SERVO_change_angle(SERVO_UPHILL);
+            FAN_set_spin(FAN_LIFT, FAN_MAX);
+            FAN_set_spin(FAN_STEER, FAN_MAX);
         }
         
     }
