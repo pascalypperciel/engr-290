925be1525e28ca026912eecf7c0fa53832f44b12
pascalypperciel <pypperciel@gmail.com>
2024-06-16 22:56:45 -0400

    wip

--- code/2 side/code2/code2.ino
+++ code/2 side/code2/code2.ino
@@ -4,12 +4,16 @@
 #include <stdbool.h>
 #include <avr/interrupt.h>
 
+#include "mpu6050.h"
+#include <Wire.h>
+
 // Values to tweak
 #define SENSOR_ITERATOR     10
 #define DISTANCE_HORIZONTAL 40.00
 #define DISTANCE_VERTICAL   80.00
 #define SIDEWAYS_DELAY_MS   2000
 #define DELAY_BEFORE_TURN   2000
+#define ERROR_TURN_YAW      15
 
 // Constants
 #define FAN_HORI_SPEED      255
@@ -17,9 +21,17 @@
 #define FAN_DOWNHILL_SPEED  200
 #define FAN_MAX             255
 #define FAN_OFF             0
-#define SERVO_UPHILL        0
-#define SERVO_SIDEWAYS      90
-#define SERVO_DOWNHILL      180
+
+#define ANGLE_SERVO_RIGHT   0
+#define ANGLE_SERVO_CENTER  90
+#define ANGLE_SERVO_LEFT    180
+
+#define ANGLE_YAW_AWAY      0
+#define ANGLE_YAW_TOWARDS   180
+
+#define PID_PROP 0.5
+#define PID_INTE 0.1
+#define PID_DIFF 2.0
 
 // Debug
 #define RUN_HOVERCRAFT      false
@@ -46,11 +58,17 @@ typedef struct {
 } FAN;
 
 // Components
-UltrasonicSensor  US_FRONT =  {PD2, PB3, "Front"};
-UltrasonicSensor  US_SIDE =   {PD3, PB5, "Side"};
+UltrasonicSensor  US_LEFT =  {PD2, PB3, "Left"};
+UltrasonicSensor  US_RIGHT =   {PD3, PB5, "Right"};
 ServoMotor        SERVO =     {PB1};
 FAN               FAN_LIFT =  {PD5};
 FAN               FAN_STEER = {PD6};
+MPU6050           MPU;
+
+// Global variables
+float i_input= 0;
+float d_last= 0;
+int target_yaw = ANGLE_YAW_AWAY;
 
 // Functions
 void UART_init() {
@@ -138,16 +156,13 @@ float SENSORS_distances_average(UltrasonicSensor US) {
     return sum / SENSOR_ITERATOR;
 }
 
-bool SENSORS_opening_detected() {
-    return SENSORS_distances_average(US_SIDE) > DISTANCE_HORIZONTAL;
-}
-
-int SENSOR_decide_angle() {
-    if (SENSORS_distances_average(US_FRONT) >= DISTANCE_VERTICAL) {
-        return SERVO_UPHILL;
-    } else {
-        return SERVO_DOWNHILL;
+int SENSORS_opening_detected() {
+    if (SENSORS_distances_average(US_RIGHT) > DISTANCE_HORIZONTAL) {
+        return ANGLE_SERVO_RIGHT;
+    } else if (SENSORS_distances_average(US_LEFT) > DISTANCE_HORIZONTAL) {
+        return ANGLE_SERVO_LEFT;
     }
+    return 0;
 }
 
 void SERVO_init_timer1() {
@@ -166,16 +181,10 @@ void SERVO_change_angle(float angle) {
     if (angle < 0) angle = 0;
     if (angle > 180) angle = 180;
 
-    OCR1A = map(angle, 0, 180, 800, 4300);
+    //OCR1A = map(angle, 0, 180, 800, 4300);
     
     if (RUN_HOVERCRAFT) {
-        if (angle == SERVO_SIDEWAYS) {
-            FAN_set_spin(FAN_STEER, FAN_HORI_SPEED);
-        } else if (angle == SERVO_UPHILL) {
-            FAN_set_spin(FAN_STEER, FAN_UPHILL_SPEED);
-        } else {
-            FAN_set_spin(FAN_STEER, FAN_DOWNHILL_SPEED);
-        }
+        FAN_set_spin(FAN_STEER, FAN_MAX);
     }
 }
 
@@ -192,36 +201,74 @@ void FAN_set_spin(FAN fan, int value) {
     }
 }
 
+void mpu6050_begin()  {
+    Wire.begin();
+    MPU.begin(); 
+}
+
+float mpu6050_yaw() {
+    MPU6050_t data = MPU.get();
+    while (data.dir.error != 0) {
+        TWCR = 0;
+        Wire.begin();
+        data = MPU.get();
+    }
+    return data.dir.yaw;
+}
+
+float PID_adjust(float error) {
+    float pid_pset = error;
+    float pid_iset = constrain(i_input+error,-50,+50);
+    float pid_dset= error-d_last; 
+    d_last = error;
+  
+    return pid_pset*PID_PROP + pid_iset*PID_INTE + pid_dset*PID_DIFF;
+}
+
+void MPU_change_target_yaw() {
+    if (target_yaw == ANGLE_YAW_AWAY) {
+        target_yaw = ANGLE_YAW_TOWARDS;
+    } else {
+        target_yaw = ANGLE_YAW_AWAY;
+    }
+}
+
+bool MPU_is_turn_over() {
+    float current_yaw = mpu6050_yaw();
+    return current_yaw >= target_yaw - ERROR_TURN_YAW && current_yaw <= target_yaw + ERROR_TURN_YAW;
+}
+
 void GENERAL_init_interrupts() {
     SREG |= (1 << 7);
 }
 
 void GENERAL_init_ports() {
     // Outputs
-    DDRB |= (1 << US_FRONT.TRIGGER_PIN);
-    DDRB |= (1 << US_SIDE.TRIGGER_PIN);
+    DDRB |= (1 << US_RIGHT.TRIGGER_PIN);
+    DDRB |= (1 << US_LEFT.TRIGGER_PIN);
     DDRB |= (1 << SERVO.INPUT_PIN);
     DDRD |= (1 << FAN_LIFT.INPUT_PIN);
     DDRD |= (1 << FAN_STEER.INPUT_PIN);
 
     //Inputs
-    DDRD &= ~(1 << US_FRONT.ECHO_PIN);
-    DDRD &= ~(1 << US_SIDE.ECHO_PIN);
+    DDRD &= ~(1 << US_RIGHT.ECHO_PIN);
+    DDRD &= ~(1 << US_LEFT.ECHO_PIN);
 }
 
 void GENERAL_setup() {
     FAN_set_spin(FAN_LIFT, FAN_OFF);
     FAN_set_spin(FAN_STEER, FAN_OFF);
     if (RUN_HOVERCRAFT) {
-        SERVO_change_angle(SERVO_UPHILL);
+        SERVO_change_angle(ANGLE_SERVO_CENTER);
         FAN_set_spin(FAN_LIFT, FAN_MAX);
+        FAN_set_spin(FAN_STEER, FAN_MAX);
     }
 }
 
-void GENERAL_move_sideways() {
-    SERVO_change_angle(SERVO_SIDEWAYS);
-    _delay_ms(SIDEWAYS_DELAY_MS);
-    SERVO_change_angle(SENSOR_decide_angle());
+void GENERAL_turn(float opening_angle) {
+    MPU_change_target_yaw();
+    SERVO_change_angle(opening_angle);
+    while(!MPU_is_turn_over()) {}
 }
 
 int main(void) {
@@ -231,18 +278,27 @@ int main(void) {
     UART_init();
     SERVO_init_timer1();
     FAN_init();
+    mpu6050_begin();
     GENERAL_setup();
-
+    
     char buffer[16];
     while (1) {
         UART_send_string("\n");
-        if (RUN_HOVERCRAFT && SENSORS_opening_detected()) {
-            GENERAL_move_sideways();
+        int opening_angle = SENSORS_opening_detected();
+        if (RUN_HOVERCRAFT && opening_angle != 0) {
+            GENERAL_turn(opening_angle);
+        }
+
+        if (RUN_HOVERCRAFT) {
+            float imu_error = target_yaw - mpu6050_yaw();
+            float angle_correct = target_yaw - PID_adjust(imu_error);
+            angle_correct = constrain(angle_correct, 0, 180);
+            SERVO_change_angle(angle_correct);
         }
 
         if (DEBUG_SENSORS) {
-            SENSORS_measure_distance(US_FRONT, true);
-            SENSORS_measure_distance(US_SIDE, true);
+            SENSORS_measure_distance(US_RIGHT, true);
+            SENSORS_measure_distance(US_LEFT, true);
         }
 
         if (DEBUG_SERVO) {
@@ -262,16 +318,16 @@ int main(void) {
         }
 
         if (DEBUG_SERVO_SPIN) {
-            SERVO_change_angle(SERVO_UPHILL);
+            SERVO_change_angle(ANGLE_SERVO_RIGHT);
             _delay_ms(2000);
-            SERVO_change_angle(SERVO_SIDEWAYS);
+            SERVO_change_angle(ANGLE_SERVO_CENTER);
             _delay_ms(2000);
-            SERVO_change_angle(SERVO_DOWNHILL);
+            SERVO_change_angle(ANGLE_SERVO_LEFT);
             _delay_ms(2000);
         }
 
         if (DEBUG_SKIRT) {
-            SERVO_change_angle(SERVO_UPHILL);
+            SERVO_change_angle(ANGLE_SERVO_CENTER);
             FAN_set_spin(FAN_LIFT, FAN_MAX);
             FAN_set_spin(FAN_STEER, FAN_MAX);
         }
