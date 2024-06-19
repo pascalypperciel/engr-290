f4bb2880dc2094f6f3b39a258816557ecc1cfa17
pascalypperciel <pypperciel@gmail.com>
2024-06-18 22:03:37 -0400

    modification

--- code/2 side/code2/code2.ino
+++ code/2 side/code2/code2.ino
@@ -8,19 +8,26 @@
 #include <Wire.h>
 
 // Values to tweak
-#define RANGE_WALL          60
-#define ERROR_TURN_YAW      50
+#define RANGE_WALL          40
+#define ERROR_TURN_YAW      0
+#define WAIT_AFTER_TURN     40
+#define ITERATOR_AVERAGE    1
+#define VALUES_IN_A_ROW     5
+
 
 // Constants
 #define FAN_SPEED_MAX       255
+#define FAN_SPEED_LESS      230
 #define FAN_SPEED_OFF       0
 
-#define ANGLE_SERVO_RIGHT   160
+#define ANGLE_SERVO_RIGHT   180
 #define ANGLE_SERVO_CENTER  90
-#define ANGLE_SERVO_LEFT    20
+#define ANGLE_SERVO_LEFT    0
 
 #define ANGLE_YAW_AWAY      0
 #define ANGLE_YAW_TOWARDS   180
+
+#define PID_PROP            0.5
 
 // Structures
 typedef struct {
@@ -41,6 +48,11 @@ HCSR04            US_RIGHT(13, 3);
 
 // Global variable
 int target_yaw = ANGLE_YAW_AWAY;
+int counter = WAIT_AFTER_TURN;
+int opening_in_a_row_right = 0;
+int opening_in_a_row_left = 0;
+bool last_was_big_right = false;
+bool last_was_big_left = false;
 
 // Functions
 void UART_init() {
@@ -64,17 +76,40 @@ void UART_send_string(const char *str) {
 
 int SENSORS_opening_detected() {
     char buffer[32];
+    
     int right = (int) US_RIGHT.dist();
     snprintf(buffer, sizeof(buffer), "Right = %d\r\t", right);
     UART_send_string(buffer);
+    
     int left = (int) US_LEFT.dist();
     snprintf(buffer, sizeof(buffer), "Left = %d\r\t", left);
     UART_send_string(buffer);
+    
     if (right >= RANGE_WALL) {
-        return ANGLE_SERVO_RIGHT;
+        opening_in_a_row_right++;
+        if (opening_in_a_row_right == VALUES_IN_A_ROW && last_was_big_right) {
+            opening_in_a_row_right = 0;
+            last_was_big_right = false;
+            return ANGLE_SERVO_RIGHT;
+        } else {
+            last_was_big_right = true;
+            return -1;
+        }
     } else if (left >= RANGE_WALL) {
-        return ANGLE_SERVO_LEFT;
+        opening_in_a_row_left++;
+        if (opening_in_a_row_left == VALUES_IN_A_ROW && last_was_big_left) {
+            opening_in_a_row_left = 0;
+            return ANGLE_SERVO_LEFT;
+        } else {
+            last_was_big_left = true;
+            last_was_big_left = false;
+            return -1;
+        }
     }
+    last_was_big_right = false;
+    last_was_big_left = false;
+    opening_in_a_row_right = 0;
+    opening_in_a_row_left = 0;
     return -1;
 }
 
@@ -122,11 +157,11 @@ float MPU_get_yaw() {
       MPU6050_t data = ICU.get();
       char buffer[16];
       float yaw = (int) data.dir.yaw;
-      if (yaw >= 360 || yaw <= -360) {
-          ICU.begin();
-          ICU.get();
-          yaw = (int) abs(data.dir.yaw);
-      }
+//      if (yaw >= 360 || yaw <= -360) {
+//          ICU.begin();
+//          ICU.get();
+//          yaw = (int) abs(data.dir.yaw);
+//      }
       dtostrf(yaw, 6, 2, buffer);
       UART_send_string("\nYaw : \t");
       UART_send_string(buffer);
@@ -170,21 +205,25 @@ void GENERAL_init_ports() {
 void GENERAL_components_setup() {
     SERVO_change_angle(ANGLE_SERVO_CENTER);
     FAN_set_spin(FAN_LIFT, FAN_SPEED_MAX);
-    FAN_set_spin(FAN_STEER, FAN_SPEED_MAX);
+    FAN_set_spin(FAN_STEER, FAN_SPEED_LESS);
 }
 
 void GENERAL_forward_logic() {
+    //this code needs to take 90 (away) or 270 (towards) yaw and orient towards that, ideally the servo stays near 90. This will run until opening is detected.
     float imu_error;
+    float yaw = MPU_get_yaw();
     if (target_yaw == ANGLE_YAW_AWAY) {
-      imu_error = 90 - (target_yaw - MPU_get_yaw());
+      imu_error = 90 - (target_yaw - yaw);
     }
     else if (target_yaw == ANGLE_YAW_TOWARDS) {
-      imu_error = 90 - (target_yaw - MPU_get_yaw());
-      
-      char buffer[32];
-      snprintf(buffer, sizeof(buffer), "\t\tANgle rn = %d\r\t", (int)imu_error);
-      UART_send_string(buffer);
+      imu_error = 90 - (target_yaw - yaw);
     }
+    
+    float PID_constant = target_yaw - yaw;
+    if(abs(PID_constant) > 5) {
+      imu_error += (-PID_constant);
+    }
+    
     SERVO_change_angle(imu_error);
 }
 
@@ -206,12 +245,22 @@ void setup() {
     int error= ICU.begin();
     if (error) { UART_send_string("MPU initialization failed :("); }
     GENERAL_components_setup();
+    counter = WAIT_AFTER_TURN;
+    opening_in_a_row_right = 0;
+    opening_in_a_row_left = 0;
+    last_was_big_right = false;
+    last_was_big_left = false;
 }
 
-void loop() {     
+void loop() {  
+    if (counter > 0) {
+        counter--;   
+    }
     GENERAL_forward_logic();
     int opening_angle = SENSORS_opening_detected();
-    if (opening_angle != -1) {
+    if (opening_angle != -1 && counter == 0) {
+        UART_send_string("TURNIIINNGGG===============");
         GENERAL_turn(opening_angle);
+        counter = WAIT_AFTER_TURN;
     }
 }
