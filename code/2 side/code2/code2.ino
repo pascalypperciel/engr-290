8a68793e0c57b68021db7d25840e0f2721f1a4cf
pascalypperciel <pypperciel@gmail.com>
2024-06-21 00:18:59 -0400

    Pure C-ify millis

--- code/2 side/code2/code2.ino
+++ code/2 side/code2/code2.ino
@@ -14,7 +14,6 @@
 #define WAIT_AFTER_TURN     50
 #define VALUES_IN_A_ROW     2
 
-
 // Constants
 #define FAN_SPEED_MAX       255
 #define FAN_SPEED_LESS      185
@@ -27,6 +26,10 @@
 
 #define ANGLE_YAW_AWAY      0
 #define ANGLE_YAW_TOWARDS   180
+
+#define MILLIS_INC 1
+#define FRACT_INC 3
+#define FRACT_MAX 1000
 
 // Structures
 typedef struct {
@@ -57,14 +60,16 @@ Gyroscope         IMU =       {PC4, PC5};
 MPU6050           ICU;
 
 // Global variable
-int target_yaw = ANGLE_YAW_AWAY;
-int counter = WAIT_AFTER_TURN;
-int opening_in_a_row_right = 0;
-int opening_in_a_row_left = 0;
-bool last_was_big_right = false;
-bool last_was_big_left = false;
-int can_i_turn_yet = 0;
-float d_last = 0;
+int _target_yaw = ANGLE_YAW_AWAY;
+int _counter = WAIT_AFTER_TURN;
+int _opening_in_a_row_right = 0;
+int _opening_in_a_row_left = 0;
+bool _last_was_big_right = false;
+bool _last_was_big_left = false;
+float _d_last = 0;
+volatile unsigned long _timer0_millis = 0;
+volatile unsigned char _timer0_fract = 0;
+static unsigned long _timer0_overflow_count = 0;
 
 // Functions
 void UART_init() {
@@ -86,7 +91,6 @@ void UART_send_string(const char *str) {
     }
 }
 
-
 float SENSORS_measure_distance(UltrasonicSensor US) {
     // Send a 10us pulse to trigger pin
     PORTB &= ~(1 << US.TRIGGER_PIN);
@@ -96,26 +100,26 @@ float SENSORS_measure_distance(UltrasonicSensor US) {
     PORTB &= ~(1 << US.TRIGGER_PIN);
 
     long distance = 0;
-    long loop_counter = 0;
+    long loop__counter = 0;
     long loop_max = (23529 * (F_CPU / 1000000)) / 16;
     bool break_all = false;
 
     while ((PIND & (1 << US.ECHO_PIN)) == (1 << US.ECHO_PIN)) {
-        if (loop_counter++ == loop_max) {
+        if (loop__counter++ == loop_max) {
             break_all = true;
             break;
         }
     }
 
     while ((PIND & (1 << US.ECHO_PIN)) != (1 << US.ECHO_PIN) && !break_all) {
-        if (loop_counter++ == loop_max) {
+        if (loop__counter++ == loop_max) {
             break_all = true;
             break;
         }
     }
 
     while ((PIND & (1 <<US.ECHO_PIN)) == (1 << US.ECHO_PIN) && !break_all) {
-        if (loop_counter++ == loop_max) {
+        if (loop__counter++ == loop_max) {
             break;
         }
         distance++;
@@ -123,7 +127,6 @@ float SENSORS_measure_distance(UltrasonicSensor US) {
 
     return (float)(distance * (16 / (F_CPU / 1000000))) / 58.8235;
 }
-
 
 int SENSORS_opening_detected() {
     char buffer[32];
@@ -138,31 +141,31 @@ int SENSORS_opening_detected() {
     
 
     if (left >= RANGE_WALL) {
-        opening_in_a_row_left++;
-        if (opening_in_a_row_left == VALUES_IN_A_ROW && last_was_big_left) {
-            opening_in_a_row_left = 0;
-            last_was_big_right = false;
+        _opening_in_a_row_left++;
+        if (_opening_in_a_row_left == VALUES_IN_A_ROW && _last_was_big_left) {
+            _opening_in_a_row_left = 0;
+            _last_was_big_right = false;
             return ANGLE_SERVO_LEFT;
         } else {
-            last_was_big_left = true;
+            _last_was_big_left = true;
             return -1;
         }
     }
     if (right >= RANGE_WALL) {
-        opening_in_a_row_right++;
-        if (opening_in_a_row_right == VALUES_IN_A_ROW && last_was_big_right) {
-            opening_in_a_row_right = 0;
-            last_was_big_right = false;
+        _opening_in_a_row_right++;
+        if (_opening_in_a_row_right == VALUES_IN_A_ROW && _last_was_big_right) {
+            _opening_in_a_row_right = 0;
+            _last_was_big_right = false;
             return ANGLE_SERVO_RIGHT;
         } else {
-            last_was_big_right = true;
+            _last_was_big_right = true;
             return -1;
         }
     }
-    last_was_big_right = false;
-    last_was_big_left = false;
-    opening_in_a_row_right = 0;
-    opening_in_a_row_left = 0;
+    _last_was_big_right = false;
+    _last_was_big_left = false;
+    _opening_in_a_row_right = 0;
+    _opening_in_a_row_left = 0;
     return -1;
 }
 
@@ -179,31 +182,31 @@ void SERVO_change_angle(float angle) {
 
     OCR1A = map(angle, 0, 180, 800, 4300);
 }
-
-void FAN_init() {
-    TCCR0A |= (1 << WGM00) | (1 << WGM01) | (1 << COM0A1) | (1 << COM0B1);
-    TCCR0B |= (1 << CS01) | (1 << CS00);
-}
-
-void FAN_set_spin(FAN fan, int value) {
-    if (fan.INPUT_PIN == PD5) {
-        OCR0B = value;
-    } else if (fan.INPUT_PIN == PD6) {
-        OCR0A = value;
-    }
-}
+//
+//void FAN_init() {
+//    TCCR0A |= (1 << WGM00) | (1 << WGM01) | (1 << COM0A1) | (1 << COM0B1);
+//    TCCR0B |= (1 << CS01) | (1 << CS00);
+//}
+//
+//void FAN_set_spin(FAN fan, int value) {
+//    if (fan.INPUT_PIN == PD5) {
+//        OCR0B = value;
+//    } else if (fan.INPUT_PIN == PD6) {
+//        OCR0A = value;
+//    }
+//}
 
 void MPU_change_target_yaw() {
-    if (target_yaw == ANGLE_YAW_AWAY) {
-        target_yaw = ANGLE_YAW_TOWARDS;
+    if (_target_yaw == ANGLE_YAW_AWAY) {
+        _target_yaw = ANGLE_YAW_TOWARDS;
     } else {
-        target_yaw = ANGLE_YAW_AWAY;
+        _target_yaw = ANGLE_YAW_AWAY;
     }
 }
 
 bool MPU_is_turn_over() {
     float current_yaw = MPU_get_yaw_turning();
-    return current_yaw >= target_yaw - ERROR_TURN_YAW && current_yaw <= target_yaw + ERROR_TURN_YAW;
+    return current_yaw >= _target_yaw - ERROR_TURN_YAW && current_yaw <= _target_yaw + ERROR_TURN_YAW;
 }
 
 float MPU_get_yaw() {
@@ -219,7 +222,7 @@ float MPU_get_yaw() {
 float MPU_get_yaw_turning() {
     MPU6050_t data = ICU.get();
     char buffer[32];
-    char target_yaw_buffer[16];
+    char _target_yaw_buffer[16];
     
     int yaw = (int) abs(data.dir.yaw);
     if (yaw >= 360 || yaw <= -360) {
@@ -229,10 +232,10 @@ float MPU_get_yaw_turning() {
     }
     
     dtostrf(yaw, 6, 2, buffer);
-    dtostrf(target_yaw, 6, 2, target_yaw_buffer);
+    dtostrf(_target_yaw, 6, 2, _target_yaw_buffer);
     
-    strcat(buffer, " target_yaw: ");
-    strcat(buffer, target_yaw_buffer);
+    strcat(buffer, " _target_yaw: ");
+    strcat(buffer, _target_yaw_buffer);
     
     UART_send_string("\nYee: \t");
     UART_send_string(buffer);
@@ -282,6 +285,25 @@ bool IMU_init() {
     int error = IMU_calibrate()
 }
 
+unsigned long GENERAL_millis()
+{
+    unsigned long m;
+    uint8_t oldSREG = SREG;
+    
+    cli();
+    m = _timer0_millis;
+    SREG = oldSREG;
+
+    return m;
+}
+
+void GENERAL_init_timer0() {
+    TCCR0A = 0;
+    TCCR0B = (1 << CS01) | (1 << CS00);
+    TIMSK0 = (1 << TOIE0);
+    sei();
+}
+
 void GENERAL_init_interrupts() {
     SREG |= (1 << 7);
 }
@@ -300,34 +322,34 @@ void GENERAL_init_ports() {
     DDRD &= ~(1 << US_RIGHT.ECHO_PIN);
     DDRD &= ~(1 << US_LEFT.ECHO_PIN);
 
-    //Set high
+    // Set high
     PORTC |= (1 << IMU.SDA_PIN);
     PORTC |= (1 << IMU.SCL_PIN);
 }
 
 void GENERAL_components_setup() {
     SERVO_change_angle(ANGLE_SERVO_CENTER);
-    FAN_set_spin(FAN_LIFT, FAN_SPEED_MAX);
-    FAN_set_spin(FAN_STEER, FAN_SPEED_LESS);
+//    FAN_set_spin(FAN_LIFT, FAN_SPEED_MAX);
+//    FAN_set_spin(FAN_STEER, FAN_SPEED_LESS);
 }
 
 void GENERAL_go_forward() {
     float imu_error;
     float yaw = MPU_get_yaw();
     float PID_constant = 0;
-    if (target_yaw == ANGLE_YAW_AWAY) {
-      imu_error = 90 - (target_yaw - yaw);
-      PID_constant = target_yaw - yaw;
-      float d_input = PID_constant - d_last;
-      d_last = PID_constant;             
+    if (_target_yaw == ANGLE_YAW_AWAY) {
+      imu_error = 90 - (_target_yaw - yaw);
+      PID_constant = _target_yaw - yaw;
+      float d_input = PID_constant - _d_last;
+      _d_last = PID_constant;             
       imu_error += (-(PID_constant * 0.5 + d_input * 2));
     }
-    else if (target_yaw == ANGLE_YAW_TOWARDS) {
-      imu_error = 90 - (target_yaw + yaw);      //(180 + 171) - 270 = 81
-      PID_constant = target_yaw + yaw;          //180 -171 = 9 
+    else if (_target_yaw == ANGLE_YAW_TOWARDS) {
+      imu_error = 90 - (_target_yaw + yaw);      //(180 + 171) - 270 = 81
+      PID_constant = _target_yaw + yaw;          //180 -171 = 9 
           if(abs(PID_constant) > 5) {
-            float d_input = PID_constant - d_last;
-            d_last = PID_constant;                  
+            float d_input = PID_constant - _d_last;
+            _d_last = PID_constant;                  
             imu_error += (PID_constant * 0.5 + d_input * 2);
           }
     }
@@ -341,45 +363,61 @@ void GENERAL_go_forward() {
 }
 
 void GENERAL_make_turn(float opening_angle) {
-    FAN_set_spin(FAN_STEER, FAN_SPEED_LESS_LESS);
+//    FAN_set_spin(FAN_STEER, FAN_SPEED_LESS_LESS);
     SERVO_change_angle(opening_angle);
     MPU_change_target_yaw();
     while (!MPU_is_turn_over());
-    FAN_set_spin(FAN_STEER, FAN_SPEED_LESS);
+//    FAN_set_spin(FAN_STEER, FAN_SPEED_LESS);
     SERVO_change_angle(90);
-    delay(100);
+    _delay_ms(100);
 }
 
-void setup() {
+int main(void) {
+    GENERAL_init_timer0();
     GENERAL_init_interrupts();
     GENERAL_init_ports();
     UART_init();
     SERVO_init_timer1();
-    FAN_init();
+//    FAN_init();
     GENERAL_TWI_init();
-    int error= ICU.begin();
+    int error = ICU.begin();
     if (error) { UART_send_string("MPU initialization failed :("); }
     GENERAL_components_setup();
     
-    counter = WAIT_AFTER_TURN;
-    opening_in_a_row_right = 0;
-    opening_in_a_row_left = 0;
-    last_was_big_right = false;
-    last_was_big_left = false;
+    _counter = WAIT_AFTER_TURN;
+    _opening_in_a_row_right = 0;
+    _opening_in_a_row_left = 0;
+    _last_was_big_right = false;
+    _last_was_big_left = false;
+
+    while (1) {
+        if (_counter > 0) {
+            _counter--;   
+        }
+        GENERAL_forward_logic();
+    
+        int opening_angle = SENSORS_opening_detected();
+        if (opening_angle != -1 && _counter == 0) {
+            GENERAL_make_turn(opening_angle);
+            _counter = WAIT_AFTER_TURN;
+            _can_i_turn_yet = 0;
+        }
+    }
 }
 
-void loop() {  
-    if (counter > 0) {
-        counter--;   
-    }
-    GENERAL_forward_logic();
-    can_i_turn_yet++;
+SIGNAL(TIMER0_OVF_vect)
+{
+    unsigned long m = _timer0_millis;
+    unsigned char f = _timer0_fract;
 
-    int opening_angle = SENSORS_opening_detected();
-    if (opening_angle != -1 && counter == 0 && can_i_turn_yet > 30) {
-        UART_send_string("TURNIIINNGGG===============");
-        GENERAL_turn(opening_angle);
-        counter = WAIT_AFTER_TURN;
-        can_i_turn_yet = 0;
+    m += MILLIS_INC;
+    f += FRACT_INC;
+    if (f >= FRACT_MAX) {
+        f -= FRACT_MAX;
+        m += 1;
     }
+
+    _timer0_fract = f;
+    _timer0_millis = m;
+    _timer0_overflow_count++;
 }
